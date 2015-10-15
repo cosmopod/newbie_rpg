913c39640b086feba2f0aacab1dbeec26607f354
cosmopod <francisco@realcomabroad.com>
2015-10-15 23:39:42 +0200

    clase player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Entity
+{
+
+	public string[] inventory;
+	public string[] skills;
+	public int money;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
